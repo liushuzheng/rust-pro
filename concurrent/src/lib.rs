0b44a549062f68df6ab0a3fc6054db0393fda059
liushuz <xiegutimo@aliyum.com>
2023-04-21 00:10:59 +0800

    update mut

---
+++ concurrent/src/lib.rs
@@ -0,0 +1,32 @@
+use std::rc::Rc;
+use std::sync::Mutex;
+use std::thread;
+
+#[test]
+fn mut_d() {
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m);
+}
+
+
+#[test]
+fn mm_d() {
+    let counter = Rc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
+}
