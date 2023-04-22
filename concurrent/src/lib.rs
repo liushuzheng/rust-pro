82be9a1b31ab0ee4fb3179d5dd9815d7f13263dd
liushuz <xiegutimo@aliyun.com>
2023-04-22 23:37:27 +0800

    concurrent finish

--- concurrent/src/lib.rs
+++ concurrent/src/lib.rs
@@ -1,5 +1,4 @@
-use std::rc::Rc;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 #[test]
@@ -15,10 +14,10 @@ fn mut_d() {
 
 #[test]
 fn mm_d() {
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
