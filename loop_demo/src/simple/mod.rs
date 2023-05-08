3c22bdeea507ee21bff63cfb4f43c3db5d22fb68
liushuz <xiegutimo@aliyun.com>
2023-05-08 14:01:06 +0800

    add leetcode module

---
+++ loop_demo/src/simple/mod.rs
@@ -0,0 +1,11 @@
+use std::iter::Map;
+
+// 给定一个整数数组 nums 和一个整数目标值 target，
+// 请你在该数组中找出 和为目标值 target 的那两个整数，
+// 并返回它们的数组下标。
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    nums.iter().map(|i|{i,i
+
+    })
+    vec![]
+}
