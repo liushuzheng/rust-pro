1a1412f462d862d66957513efec789846e98ec02
liushuz <xiegutimo@aliyum.com>
2023-05-09 23:05:30 +0800

    修改索引

--- leetcode/src/simple/mod.rs
+++ leetcode/src/simple/mod.rs
@@ -23,7 +23,7 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
                 continue;
             }
             Some(index) => {
-                return vec![*x.1, *index as i32];
+                return vec![x.0 as i32, *index as i32];
             }
         }
     }
@@ -33,7 +33,7 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
 #[test]
 fn test1() {
-    let index = two_sum(vec![2, 7, 11, 15], 9);
+    let index = two_sum(vec![3,2,4], 6);
     for x in index {
         println!("index is {}", x);
     }
